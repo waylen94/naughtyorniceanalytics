414609860d9325f8574367568a1c7d5ed588e48b
Weilun Liu <763027562@qq.com>
2019-05-17 00:19:23 +0000

    initiate the initial page

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="{{ app()->getLocale() }}">
+
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <!-- CSRF Token -->
+  <meta name="csrf-token" content="{{ csrf_token() }}">
+  <title>@yield('title', 'Analytics') - NaughtyorNice</title>
+  <!-- Styles -->
+  <link href="{{ mix('css/app.css') }}" rel="stylesheet">
+</head>
+
+<body>
+  <div id="app" class="{{ route_class() }}-page">
+  <!-- Page Wrapper -->
+  	<div id="wrapper">
+
+        @include('layouts._sidebar')
+    
+
+        <!-- Content Wrapper -->
+        <div id="content-wrapper" class="d-flex flex-column">
+        @include('layouts._header')
+        @yield('content')
+        @include('layouts._footer')
+        </div>
+    <!-- End of Content Wrapper -->
+  	</div>
+  </div>
+  <!-- End of Page Wrapper -->
+  
+  
+
+  <!-- Scripts -->
+  <script src="{{ mix('js/app.js') }}"></script>
+</body>
+
+</html>
---
+++ resources/views/pages/root.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.app')
+@section('title', 'homepage')
+@section('content')
+  <h1>this is home page</h1>
+@stop
