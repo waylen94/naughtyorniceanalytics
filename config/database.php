01faa7d44b6141616d91df9a1fcac0b57d8328c5
Weilun Liu <763027562@qq.com>
2019-05-23 05:27:07 +0000

    Updata database configuration

--- config/database.php
+++ config/database.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Str;
 
+$db_config = get_db_config();
+
 return [
 
     /*
@@ -15,8 +17,8 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'mysql'),
-
+//     'default' => env('DB_CONNECTION', 'mysql'),
+    'default' => $db_config['connection'],
     /*
     |--------------------------------------------------------------------------
     | Database Connections
@@ -37,7 +39,7 @@ return [
 
         'sqlite' => [
             'driver' => 'sqlite',
-            'url' => env('DATABASE_URL'),
+//             'url' => env('DATABASE_URL'),
             'database' => env('DB_DATABASE', database_path('database.sqlite')),
             'prefix' => '',
             'foreign_key_constraints' => env('DB_FOREIGN_KEYS', true),
@@ -64,18 +66,28 @@ return [
         ],
 
         'pgsql' => [
-            'driver' => 'pgsql',
-            'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
-            'charset' => 'utf8',
-            'prefix' => '',
-            'prefix_indexes' => true,
-            'schema' => 'public',
-            'sslmode' => 'prefer',
+//             'driver' => 'pgsql',
+//             'url' => env('DATABASE_URL'),
+//             'host' => env('DB_HOST', '127.0.0.1'),
+//             'port' => env('DB_PORT', '5432'),
+//             'database' => env('DB_DATABASE', 'forge'),
+//             'username' => env('DB_USERNAME', 'forge'),
+//             'password' => env('DB_PASSWORD', ''),
+//             'charset' => 'utf8',
+//             'prefix' => '',
+//             'prefix_indexes' => true,
+//             'schema' => 'public',
+//             'sslmode' => 'prefer',
+            'driver'   => 'pgsql',
+            'host'     => $db_config['host'],
+            'port'     => env('DB_PORT', '5432'),
+            'database' => $db_config['database'],
+            'username' => $db_config['username'],
+            'password' => $db_config['password'],
+            'charset'  => 'utf8',
+            'prefix'   => '',
+            'schema'   => 'public',
+            'sslmode'  => 'prefer',
         ],
 
         'sqlsrv' => [
