ae4634d54df1f0289af5cb9764b886b925921eb9
Weilun Liu <763027562@qq.com>
2019-08-16 06:06:27 +0000

    news content page

--- resources/views/news/show.blade.php
+++ resources/views/news/show.blade.php
@@ -1,54 +1,59 @@
 @extends('layouts.app')
 
+@section('title', $news->title)
+
 @section('content')
 
-<div class="container">
-  <div class="col-md-10 offset-md-1">
-    <div class="card ">
-      <div class="card-header">
-        <h1>News / Show #{{ $news->id }}</h1>
-      </div>
+  <div class="row">
 
-      <div class="card-body">
-        <div class="card-block bg-light">
-          <div class="row">
-            <div class="col-md-6">
-              <a class="btn btn-link" href="{{ route('news.index') }}"><- Back</a>
-            </div>
-            <div class="col-md-6">
-              <a class="btn btn-sm btn-warning float-right mt-1" href="{{ route('news.edit', $news->id) }}">
-                Edit
+    <div class="col-lg-3 col-md-3 hidden-sm hidden-xs author-info">
+      <div class="card ">
+        <div class="card-body">
+          <div class="text-center">
+            Editor：{{ $news->user->name }}
+          </div>
+          <hr>
+          <div class="media">
+            <div align="center">
+              <a href="{{ route('users.show', $news->user->id) }}">
+                <img class="thumbnail img-fluid" src="{{ $news->user->avatar }}" width="300px" height="300px">
               </a>
             </div>
           </div>
         </div>
-        <br>
+      </div>
+    </div>
 
-        <label>Title</label>
-<p>
-	{{ $news->title }}
-</p> <label>Body</label>
-<p>
-	{{ $news->body }}
-</p> <label>User_id</label>
-<p>
-	{{ $news->user_id }}
-</p> <label>View_count</label>
-<p>
-	{{ $news->view_count }}
-</p> <label>Order</label>
-<p>
-	{{ $news->order }}
-</p> <label>Excerpt</label>
-<p>
-	{{ $news->excerpt }}
-</p> <label>Slug</label>
-<p>
-	{{ $news->slug }}
-</p>
+    <div class="col-lg-9 col-md-9 col-sm-12 col-xs-12 topic-content">
+      <div class="card ">
+        <div class="news-body">
+          <h1 class="text-center mt-3 mb-3">
+            {{ $news->title }}
+          </h1>
+
+          <div class="article-meta text-center text-secondary">
+            {{ $news->created_at->diffForHumans() }}
+            ⋅
+<!--             <i class="far fa-comment"></i> -->
+<!--             {{ $news->reply_count }} -->
+          </div>
+
+          <div class="topic-body mt-4 mb-4">
+            {!! $news->body !!}
+          </div>
+
+          <div class="operate">
+            <hr>
+            <a href="{{ route('news.edit', $news->id) }}" class="btn btn-outline-secondary btn-sm" role="button">
+              <i class="far fa-edit"></i> Modify
+            </a>
+            <a href="#" class="btn btn-outline-secondary btn-sm" role="button">
+              <i class="far fa-trash-alt"></i> Delete
+            </a>
+          </div>
+
+        </div>
       </div>
     </div>
   </div>
-</div>
-
-@endsection
+@stop
