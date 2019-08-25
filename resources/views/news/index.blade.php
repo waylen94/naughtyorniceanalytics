a2fba3a3616febee7c8362b3e2a98d85c0251ef6
Weilun Liu <763027562@qq.com>
2019-08-25 02:11:14 +0000

    tring to fix the bug in exhibition php field can not be operated in cloud 005

--- resources/views/news/index.blade.php
+++ resources/views/news/index.blade.php
@@ -2,15 +2,7 @@
 @section('title', 'News & Tips')
 @section('content')
 <div class="container">
-  <div class="col-md-10 offset-md-1">
-  @include('news._news_exhibition_list',['news'=> $news])
-          <div class="mt-5">
-          {!! $news->appends(Request::except('page'))->render() !!}
-        </div>
-        
-        
-        <!-- only for administration sara -->
-  </div>
+<h1>Testing testing</h1>
 </div>
 
 @endsection
