88288aa9aa025b8f0c619ecd881394b1a54afb0d
Weilun Liu <763027562@qq.com>
2019-08-25 01:49:03 +0000

    tring to fix the bug in exhibition php field can not be operated in cloud 003

--- resources/views/news/index.blade.php
+++ resources/views/news/index.blade.php
@@ -10,23 +10,6 @@
         
         
         <!-- only for administration sara -->
-<!--     <div class="card "> -->
-<!--       <div class="card-header"> -->
-<!--         <h1> -->
-<!--           News -->
-<!--           <a class="btn btn-success float-xs-right" href="{{ route('news.create') }}">Create</a> -->
-<!--         </h1> -->
-<!--       </div> -->
-
-<!-- 		<div class="card-body"> -->
-        
-<!--         @include('news._news_list', ['news' => $news]) -->
-       
-<!--         <div class="mt-5"> -->
-<!--           {!! $news->appends(Request::except('page'))->render() !!} -->
-<!--         </div> -->
-<!--       </div> -->
-<!--     </div> -->
   </div>
 </div>
 
